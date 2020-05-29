1f2877f8b3c86372e76fcd8428f6fddd75daba6b
Yi Ming Liu <yiming.liu02@sap.com>
2020-05-29 14:10:53 +0800

    initial commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# products
+the demo app for tutorial https://developers.sap.com/mission.sapui5-cf-launchpad.html
+
+
+## Credits
+This project has been generated with 💙 and [easy-ui5](https://github.com/SAP)
---
+++ webapp/controller/Products.controller.js
@@ -0,0 +1,22 @@
+sap.ui.define([
+  "tutorial/products/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("tutorial.products.controller.Products", {
+    onInit: function () {
+      this.addHistoryEntry({
+        title: "All Products",
+        icon: "sap-icon://product",
+        intent: "#display-data"
+      }, true);
+    },
+    handleListItemPress: function (oEvent) {
+      var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+      var selectedProductId = oEvent.getSource().getBindingContext().getProperty("ProductID");
+      oRouter.navTo("ProductDetail", {
+        productId: selectedProductId
+      });
+    }
+  });
+});
